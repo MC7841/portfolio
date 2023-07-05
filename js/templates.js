ef7c132b78d8ea1350373b3e4beb2ca79ebe9162
MC7841 <mc6428@hotmail.com>
2023-07-05 19:21:55 -0400

    Base files + code for header and template file

---
+++ js/templates.js
@@ -0,0 +1,14 @@
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+
+header.innerHTML = '<h3>My Portfolio</h3>'+
+                    '<nav>'+
+                        '<div><a href="#">Home</a></div>'+
+                        '<p>&#9679;</p>'+
+                        '<div><a href="#">Work</a></div>'+
+                        '<p>&#9679;</p>'+
+                        '<div><a href="#">About</a></div>'+
+                        '<p>&#9679;</p>'+
+                        '<div><a href="#">Contact</a></div>'+
+                    '</nav>';
+//footer.innerHTML = '';
