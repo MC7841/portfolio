fac45ff8afd8122c70560b9973b2472f6ebde810
MC7841 <mc6428@hotmail.com>
2023-07-06 21:40:06 -0400

    Created new sections and added some links to jump to sections

--- js/templates.js
+++ js/templates.js
@@ -3,12 +3,12 @@ const footer = document.getElementById("footer");
 
 header.innerHTML = '<h3>My Portfolio</h3>'+
                     '<nav>'+
-                        '<div><a href="#">Home</a></div>'+
+                        '<div><a href="#top">Home</a></div>'+
                         '<p>&#9679;</p>'+
-                        '<div><a href="#">Work</a></div>'+
+                        '<div><a href="#work">Work</a></div>'+
                         '<p>&#9679;</p>'+
-                        '<div><a href="#">About</a></div>'+
+                        '<div><a href="#about">About</a></div>'+
                         '<p>&#9679;</p>'+
-                        '<div><a href="#">Contact</a></div>'+
+                        '<div><a href="#contact">Contact</a></div>'+
                     '</nav>';
 //footer.innerHTML = '';
