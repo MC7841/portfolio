3b2a0bbd0419997cbf666c15be270c2089f4ef55
MC7841 <mc6428@hotmail.com>
2023-07-18 15:23:13 -0400

    Added descriptions to cards and removed some text

--- js/templates.js
+++ js/templates.js
@@ -5,7 +5,7 @@ header.innerHTML = '<h3>My Portfolio</h3>'+
                     '<nav>'+
                         '<a href="#top"><div>Home</div></a>'+
                         '<p>&#9679;</p>'+
-                        '<a href="#work"><div>Work</div></a>'+
+                        '<a href="#work"><div>Experience</div></a>'+
                         '<p>&#9679;</p>'+
                         '<a href="#about"><div>About</div></a>'+
                         '<p>&#9679;</p>'+
