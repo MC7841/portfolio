0974ab84321f8294656f7d128c15255a8324ceb9
MC7841 <mc6428@hotmail.com>
2023-07-20 14:38:29 -0400

    Removed remove.js and added class to template.js

--- js/templates.js
+++ js/templates.js
@@ -4,11 +4,11 @@ const footer = document.getElementById("footer");
 header.innerHTML = '<h3>My Portfolio</h3>'+
                     '<nav>'+
                         '<a href="#top"><div>Home</div></a>'+
-                        '<p>&#9679;</p>'+
+                        '<p class="dot">&#9679;</p>'+
                         '<a href="#work"><div>Experience</div></a>'+
-                        '<p>&#9679;</p>'+
+                        '<p class="dot">&#9679;</p>'+
                         '<a href="#about"><div>About</div></a>'+
-                        '<p>&#9679;</p>'+
+                        '<p class="dot">&#9679;</p>'+
                         '<a href="#contact"><div>Contact</div></a>'+
                     '</nav>';
 //footer.innerHTML = '';
